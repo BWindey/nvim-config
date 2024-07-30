345e1b7e7ac83e3a7a1133d1434e49f6399b18f1
Bram Windey <windey.bram@gmail.com>
2024-07-30 15:15:37 +0200

    Add keybinds for the todo-comments so I can open all the todo's in a quickfix-list, or search through them with telescope

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -3,5 +3,18 @@ return {
 		"folke/todo-comments.nvim",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		opts = {},
+
+		config = function ()
+			local wk = require("which-key")
+
+			wk.add({
+				mode = 'n',
+				{ "<leader>t", group = "Todo's..." },
+				{ "<leader>td", "<CMD>TodoQuickFix<CR>", desc = "QuickFix-list with all Todo's" },
+				{ "<leader>tt", "<CMD>TodoTelescope<CR>", desc = "Telescope with all Todo's" },
+			})
+
+			require("todo-comments").setup()
+		end
 	}
 }

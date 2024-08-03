811455f20a5c51c91390dd980872a0cfb406df9e
Bram Windey <windey.bram@gmail.com>
2024-08-03 21:35:23 +0200

    Add gobble to the Discord-command, used from the nvim-silicon implementation

--- lua/functions.lua
+++ lua/functions.lua
@@ -1,3 +1,28 @@
+local function gobble(lines)
+	-- Taken from "michealrommel/nvim-silicon/lua/nvim-silicon/utils.lua"
+	local shortest_whitespace = nil
+	local whitespace = ""
+	for _,v in pairs(lines) do
+		_,_, whitespace = string.find(v, "^(%s*)")
+		if type(whitespace) ~= "nil" then
+			if shortest_whitespace == nil or (#whitespace < #shortest_whitespace and v ~= "") then
+				shortest_whitespace = whitespace
+			end
+		end
+	end
+
+	if #shortest_whitespace > 0 then
+		local newlines = {}
+		for _,v in pairs(lines) do
+			local newline = string.gsub(v, "^" .. shortest_whitespace, "", 1)
+			table.insert(newlines, newline)
+		end
+		return newlines
+	else
+		return lines
+	end
+end
+
 function Discord()
 	-- Get file extension based on buffer's filetype
 	local extension = vim.fn.expand("%:e")
@@ -10,6 +35,15 @@ function Discord()
 
 	-- Surround selected text with code block
 	local selected_text = vim.fn.getreg('"')
+
+	-- Gobble longest common whitespace
+	local lines = {}
+	for line in string.gmatch(selected_text .. '\n',  "(.-)\n") do
+		table.insert(lines, line)
+	end
+	lines = gobble(lines)
+	selected_text = table.concat(lines, "\n")
+
 	local code_block = "```" .. extension .. "\n" .. selected_text .. "```\n"
 
 	-- Copy code block to clipboard

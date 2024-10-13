4be978e82449ba419cfcfbf0f53205034c2d2f19
Bram Windey <windey.bram@gmail.com>
2024-10-13 10:44:40 +0200

    Updated autoclose-plugin config to disable when touching a word, and to pair spaces, and to add a new pattern "/**/"

--- lua/plugins/autoclose.lua
+++ lua/plugins/autoclose.lua
@@ -2,7 +2,22 @@ return {
 	{
 		"m4xshen/autoclose.nvim",
 		config = function()
-			require("autoclose").setup()
+			require("autoclose").setup({
+				options = {
+					disable_when_touch = true,
+					pair_spaces = true,
+				},
+				keys = {
+					["/*"] = {
+						escape = true,
+						close = true,
+						pair = "/* */",
+						enabled_filetypes = {
+							"c", "cpp", "h",
+						}
+					},
+				}
+			})
 		end,
 	},
 }

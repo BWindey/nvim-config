ef54ceab109504b83a6e584f2396e92d1b8fb4f0
Bram Windey <windey.bram@gmail.com>
2024-11-03 13:23:40 +0100

    Add quickfix-list keybinds under '<leader>q'

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -14,21 +14,31 @@ map('i', '<C-f>', '<C-x><C-f>')
 -- Escape terminal mode easily
 map('t', '<Esc>', [[<C-\><C-n>]])
 
+local function do_vertical()
+	local hor_space = vim.api.nvim_win_get_width(0)
+	local ver_space = vim.api.nvim_win_get_height(0)
+	return hor_space > ver_space * 3
+end
+
 local wk = require("which-key")
 wk.add({
 	mode = 'n',
 	{ "<leader>o", group = "Open..." },
 	{
 		"<leader>ot", function ()
-			local hor_space = vim.api.nvim_win_get_width(0)
-			local ver_space = vim.api.nvim_win_get_height(0)
-			if hor_space < ver_space * 3 then
-				vim.cmd("botright split +terminal")
-			else
+			if do_vertical() then
 				vim.cmd("botright vsplit +terminal")
+			else
+				vim.cmd("botright split +terminal")
 			end
 			vim.cmd.startinsert()
 		end, desc = "Open terminal where there is most space"
 	},
-	{ "<leader>oq", vim.cmd.copen, desc = "Open quickfix list" }
+	{ "<leader>oq", vim.cmd.copen, desc = "Open quickfix list" },
+
+	{ "<leader>q", group = "Quickfix-list" },
+	{ "<leader>qo", vim.cmd.copen, desc = "Open quickfix list" },
+	{ "<leader>qc", vim.cmd.cclose, desc = "Open quickfix list" },
+	{ "<leader>qn", vim.cmd.cnext, desc = "Go to next quickfix item" },
+	{ "<leader>qp", vim.cmd.cprevious, desc = "Go to previous quickfix item" },
 })

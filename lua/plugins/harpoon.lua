2c609ab06055f81815f5da2717f919d3b9284cc9
Bram Windey <windey.bram@gmail.com>
2024-12-12 19:47:02 +0100

    Created function to send items from Telescope to Harpoon, and added keybinds. Still not completely how it should work, because I still get an error

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,3 +1,49 @@
+local entry_to_harpoon_item = function(entry)
+	local text = entry.text
+
+	if not text then
+		if type(entry.value) == "table" then
+			text = entry.value.text
+		else
+			text = entry.value
+		end
+	end
+
+	local file_path = require("telescope.from_entry").path(entry, false, false)
+
+	return {
+		value = file_path,
+		context = { row = 0, col = 0 },
+	}
+end
+
+local function send_selected_to_harpoon(prompt_bufnr)
+	local harpoon = require("harpoon")
+	local actions = require "telescope.actions"
+	local action_state = require "telescope.actions.state"
+	local picker = action_state.get_current_picker(prompt_bufnr)
+
+	for _, entry in ipairs(picker:get_multi_selection()) do
+		harpoon:list():add(entry_to_harpoon_item(entry))
+	end
+
+	actions.close(prompt_bufnr)
+end
+
+local function send_all_to_harpoon(prompt_bufnr)
+	local harpoon = require("harpoon")
+	local actions = require "telescope.actions"
+	local action_state = require "telescope.actions.state"
+	local picker = action_state.get_current_picker(prompt_bufnr)
+	local manager = picker.manager
+
+	for entry in manager:iter() do
+		harpoon:list():add(entry_to_harpoon_item(entry))
+	end
+
+	actions.close(prompt_bufnr)
+end
+
 return {
 	{
 		"ThePrimeagen/harpoon",
@@ -41,6 +87,20 @@ return {
 			vim.keymap.set("n", "<C-7>", function() harp:list():select(7) end)
 			vim.keymap.set("n", "<C-8>", function() harp:list():select(8) end)
 			vim.keymap.set("n", "<C-9>", function() harp:list():select(9) end)
+
+			-- Add mappings to Telescope to send things to Harpoon
+			require("telescope").setup({
+				defaults = {
+					mappings = {
+						n = {
+							["<C-s>"] = send_selected_to_harpoon
+						},
+						i = {
+							["<C-a>"] = send_all_to_harpoon
+						}
+					}
+				}
+			})
 		end,
 	},
 }

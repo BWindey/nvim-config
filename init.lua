173aa8dcc10d4b0475341fef3049edcde4619e41
Bram Windey <windey.bram@gmail.com>
2025-01-17 19:56:00 +0100

    Add filetypes for rasi and C3 so that tools like Treesitter properly work in these files, and add the treesitter parser for C3

--- init.lua
+++ init.lua
@@ -32,6 +32,15 @@ vim.opt.signcolumn = "number"
 
 vim.opt.laststatus = 3		-- Only show 1 status-line regardless of window-count
 
+-- Fix for rasi files (Rofi) and C3 files
+vim.filetype.add {
+	extension = {
+		rasi = 'rasi',
+		c3 = "c3",
+		c3i = "c3",
+		c3t = "c3",
+	},
+}
 -- Load the plugins first so I can use them (which-key) in the next steps
 require("config.lazy")
 
@@ -40,3 +49,13 @@ require("autocommands")
 require("functions")
 require("folds")
 require("keybinds")
+
+-- Install C3 treesitter
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.c3 = {
+	install_info = {
+		url = "https://github.com/c3lang/tree-sitter-c3",
+		files = { "src/parser.c", "src/scanner.c" },
+		branch = "main",
+	},
+}

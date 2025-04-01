61eacce21f8071fcb4b60fd722a0ec8ef260c8ab
Bram Windey <windey.bram@gmail.com>
2025-04-01 11:29:11 +0200

    Add hacky fix for double treesitter border

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -126,3 +126,17 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 		vim.opt.expandtab = true
 	end
 })
+
+-- HACK: until telescope updates
+vim.api.nvim_create_autocmd("User", {
+	pattern = "TelescopeFindPre",
+	callback = function()
+		vim.opt_local.winborder = "none"
+		vim.api.nvim_create_autocmd("WinLeave", {
+			once = true,
+			callback = function()
+				vim.opt_local.winborder = "rounded"
+			end,
+		})
+	end,
+})

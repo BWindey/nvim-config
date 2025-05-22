fab74bd02c08d203456ff29e2861b58588a3a31e
Bram Windey <windey.bram@gmail.com>
2025-05-22 11:43:45 +0200

    Add new C3 treesitter queries for indentation and injection

---
+++ queries/c3/indents.scm
@@ -0,0 +1,21 @@
+[
+  (compound_stmt)
+  (while_stmt)
+  (enum_body)
+  (switch_body)
+  (initializer_list)
+  (struct_body)
+  (case_stmt)
+  (default_stmt)
+  (call_invocation)
+] @indent.begin
+
+[
+  ")"
+  "}"
+] @indent.branch @indent.end
+
+[
+  (block_comment)
+  (doc_comment)
+] @indent.auto
